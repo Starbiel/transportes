734ea9f7ae8f83a55ed83ae209f24a3620385c0b
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-01-24 23:09:42 -0300

    nothing

--- php/shipping/closeShipping.php
+++ php/shipping/closeShipping.php
@@ -15,13 +15,17 @@ $validator = (new Validation);
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     if($validator->basicChecker($_POST['shippingId'])) {
         $shippingId = $_POST['shippingId'];
+        $driverPayment = str_replace('.', "", (explode('R$', $_POST['driverPayment'])[1]));
+        $driverPayment = str_replace(",", '.', $driverPayment);
+        $truckPart = str_replace('.', "", (explode('R$', $_POST['truckPart'])[1]));
+        $truckPart = str_replace(",", '.', $truckPart);
         date_default_timezone_set('America/Sao_Paulo');
         $today = date("Y-m-d H:i:s");  
-        $sql = "UPDATE shipping SET finalizado = 1, final = '$today' WHERE id_shipping = ? ";
+        $sql = "UPDATE shipping SET finalizado = 1, final = '$today', driverPayment = ?, truckPart = ? WHERE id_shipping = ? ";
         $statement = $conn->prepare($sql);
-        $statement->bind_param("i", $shippingId);
+        $statement->bind_param("ddi", $driverPayment, $truckPart, $shippingId);
         $statement->execute() or die("<b>Error:</b> Problema ao finalizar<br/>" . mysqli_connect_error()); 
-        echo "Feito : $shippingId";
+        echo 'Feito';
         $conn->close();
     }
 }
--- php/shipping/shipping.js
+++ php/shipping/shipping.js
@@ -14,21 +14,30 @@ function creatorCallback(mutationsList, observer) {
             const myNewElement = document.querySelector('.acceptButton');
             if (myNewElement) {
                 myNewElement.addEventListener('click', function() {
-                    let data = new FormData();
-                    data.append('shippingId', this.id);
-                    fetch('closeShipping.php', {
-                        method: 'POST',
-                        body: data
-                    })
-                    .then(reponse => reponse.text())
-                    .then(data => {
-                        document.querySelector("#confirmBack").remove();
-                        for (let index = 0; index < closeShipping.length; index++) {
-                            if(closeShipping[index].id == this.id) {
-                                closeShipping[index].parentElement.parentElement.parentElement.parentElement.parentElement.remove();
-                            }
+                    for (let index = 0; index < closeShipping.length; index++) {
+                        if(closeShipping[index].id == this.id) {
+                            const driverPayment = (closeShipping[index].parentElement.previousElementSibling.previousElementSibling.previousElementSibling);
+                            const truckPart = driverPayment.nextElementSibling;
+                            let data = new FormData();
+                            data.append('shippingId', this.id);
+                            data.append('driverPayment', driverPayment.innerHTML);
+                            data.append('truckPart', truckPart.innerHTML);
+                            fetch('closeShipping.php', {
+                                method: 'POST',
+                                body: data
+                            })
+                            .then(reponse => reponse.text())
+                            .then(data => {
+                                console.log(data);
+                                document.querySelector("#confirmBack").remove();
+                                for (let index = 0; index < closeShipping.length; index++) {
+                                    if(closeShipping[index].id == this.id) {
+                                        closeShipping[index].parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+                                    }
+                                }
+                            })
                         }
-                    })
+                    }
                 })
             }
         }
